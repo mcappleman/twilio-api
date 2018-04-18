caaf3b6c37cad60b00f0668eea99e5e0cd1a9904
Matt Cappleman <matt.cappleman@gmail.com>
2018-04-18 19:46:27 +0000

    Updating docker build script

--- BuildDocker.sh
+++ BuildDocker.sh
@@ -1,2 +1,2 @@
-docker build -t twilio . &&
-docker run --env-file ./.env --publish 127.0.0.1:8090:8080 --name test --rm twilio
+docker build -t twilio-api . &&
+docker run --env-file ./.env --publish 127.0.0.1:8090:8080 --name twilio --rm twilio
